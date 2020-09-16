7f6a7ac6e6b3b47dfeda4b52aff2d4da3d7078c2
COMPUTER WORLD <COMPUTER WORLD@ThisPC>
2020-09-16 08:45:26 +0530

    change by me

--- GitDemoProject_16-09/src/A.java
+++ GitDemoProject_16-09/src/A.java
@@ -1,7 +1,10 @@
 
 public class A {
 
-	
+	public void sum()
+	{
+		System.out.println("sum===0");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to junnos heaven");
 	}
