52a678e3e8a65f637b7a0bdd2da820183edc2b9c
arjunlove111 <71362186+arjunlove111@users.noreply.github.com>
2020-09-16 08:54:45 +0530

    Update A.java
    FROM git account

--- GitDemoProject_16-09/src/A.java
+++ GitDemoProject_16-09/src/A.java
@@ -3,8 +3,13 @@ public class A {
 
 	public void sum()
 	{
-		System.out.println("sum===0");
+		System.out.println("sum===A");
 	}
+	public void sub()
+	{
+		System.out.println("sub===A");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to junnos heaven");
 	}
